842fe6c082dcd502bf8e2cf0d051ab0804ec56f6
Agata Runowska-McMillan <agata@Agatas-MacBook-Air.local>
2023-06-20 20:12:52 +0100

    replaced backup

--- extra-stored-function-january-sale.sql
+++
@@ -1,4 +0,0 @@
--- Create a stored function. The company is running a January Sale where holidaymakers
--- can get a 10% discount on their holiday. The stored function will find the sale
--- price for the holiday. - Agata
-
